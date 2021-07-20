6cd4ca3ed41d8e55e66b179b71df180251fd14b8
visheshjindal8899 <visheshjindal8899@gmail.com>
2021-07-20 11:02:05 +0530

    config firebase with project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import './index.css';
 import App from './App';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlwzlu_hqvkMpogM_5FQL7sAjxnbvHADU',
+  authDomain: 'cart-819e7.firebaseapp.com',
+  projectId: 'cart-819e7',
+  storageBucket: 'cart-819e7.appspot.com',
+  messagingSenderId: '572190902338',
+  appId: '1:572190902338:web:0a9b9c0194f05563da05db',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
